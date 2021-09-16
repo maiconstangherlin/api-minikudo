9bc2b778a0ff96748b1c57e1dc9ba8fc60859b06
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2021-09-15 22:15:46 -0300

    Initial commit

---
+++ src/main/java/com/eleeme/apiminikudo/ApiMinikudoApplication.java
@@ -0,0 +1,13 @@
+package com.eleeme.apiminikudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMinikudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMinikudoApplication.class, args);
+	}
+
+}
