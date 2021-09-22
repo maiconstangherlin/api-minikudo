dd02df5759b8c0378406927cfe2d7293e58d0cd2
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2021-09-21 23:06:18 -0300

    Add Swagger base Add mongodb uri connect

--- src/main/java/com/eleeme/apiminikudo/ApiMinikudoApplication.java
+++ src/main/java/com/eleeme/apiminikudo/ApiMinikudoApplication.java
@@ -2,12 +2,15 @@ package com.eleeme.apiminikudo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiMinikudoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiMinikudoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMinikudoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/eleeme/apiminikudo/model/KudoCard.java
@@ -0,0 +1,6 @@
+package com.eleeme.apiminikudo.model;
+
+import javax.persistence.Entity;
+
+public class KudoCard {
+}
